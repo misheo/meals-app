483f36953a70b0f18447c7e886617e69254e1050
Said Nassar <saidnassar18@gmail.com>
2023-12-24 14:14:17 +0200

    fix spelling issue

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:rev_2024/Modules/category.dart';
-import 'package:rev_2024/screens/categoeies.dart';
-import 'package:rev_2024/screens/meals_screen.dart';
+import 'package:rev_2024/screens/categories.dart';
 import 'package:rev_2024/widgets/main_drawer.dart';
 
 class Tabs extends StatelessWidget {
   const Tabs({Key? key }) : super(key: key);
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title:const Text('Tab'),
       ),
       drawer: const MainDrawer() , 
-      body:  Categories(  )
+      body: const Categories(  )
     );
   }
 }
